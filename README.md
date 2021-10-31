7ac277497added743c02164ada12ec8de366d69f
Nathaniel-James <nathanjamesdev@gmail.com>
2021-10-31 18:30:21 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# MassRename
+
+## About
+MassRename is a command line tool for renaming batches of files. This removes the repetitive hassle of using your operating system's file explorer to manually rename the files.
+
+I created the tool for my own personal use so I could quickly rename episodes of TV shows along with the subtitle files.
+
+## How to install
+Simply download the python file and run it using a Terminal - it will rename files from whatever directory the file is in.
+
+## How to use 
+Simply type "massrename.py" into the terminal and give your arguments. Make sure that massrename.py is in the directoy with the files you want to rename.
+
+Example:
+```
+massrename.py "showname - Episode {n}" --filetype mp4 --includes "showname ep-"
+```
+This command will rename every `.mp4` file that starts with `"showname ep-"` to `"showname - Episode {episode number}"`.
+
+### Arguments
+Here is a list of every argument you can use in the application. They are split into 3 lists, "filter arguments" (which filter the files effected by the massrename), "other arguments" which effects various other areas of how the application works and "name arguments" which will effect how files are named.
+
+#### Filter arguments
+
+Argument | Shorthand | Use | Allows multiple inputs | Notes
+--- | --- | --- | --- | ---
+name | N/A | The name you want to rename the files to | No | **Required**, this always comes first. See [name arguments](name_arguments) for details on how to format this argument
+--filetype | -f | Filters to files that use the extentions specified | Yes | 
+--includes | -i | Filters to files that include `-includes` | Yes | If your check includes spaces then put it in quotation marks 
+
+If you want to define multiple checks (e.g. looking for files with both the `.srt` and `.mp4` extension) then add a space in between the arguments.
+
+Example:
+```
+massrename --filetype srt mp4
+```
+
+
+#### Other arguments
+
+Argument | Shorthand | Use | Allows multiple inputs | Notes
+--- | --- | --- | --- | ---
+--show-output | N/A | Shows various output messages - useful if massrename isn't filtering files how you expect it to | Takes no input
+
+#### Name arguments
+These can be used in the first "name" argument. If you don't have any of these arguments then renaming more than one file will be impossible as there will be no variation in filename, because of this massrename will automatically add a `(n)` at the end of the file (which operates in the exact same way that {n} does).
+
+Character(s) | Use | Notes
+--- | --- | ---
+{n} | Incremental number (+1) | 
+
+---
+
+## License
+Massrename uses the MIT license, see LICENSE for more details
---
+++ massrename.py
@@ -0,0 +1,148 @@
+import argparse
+import os
+import re
+
+def output_msg(msg):
+    """
+    Checks if the flag --show-output is present and outputs a debug message
+
+    Parameters:
+        msg : str
+            The message to display
+    """
+
+    if args.show_output: print(msg)
+    
+
+def filter_files(unfiltered_files, check, part):
+    """
+    Uses a list of filenames and filters them using the arguments processed by argsparse. 
+    Files that don't match the check don't get appened to the filtered list, and an output_msg() is shown.
+
+    Parameters:
+        unfiltered_files : list
+            A list of unfiltered filenames 
+        check : str
+            If this string is in a filename, it is appened to filtered
+        part : int
+            Part of the whole filename to check (0=filename, 1=file ext)
+
+    Returns: 
+        filtered_files : list
+            A list of filtered filenames
+        unfiltered_files : list
+            In the case where check doesn't exist (=None), it will just return the unfiltered_files
+    """
+
+    if check != None:
+        filtered_files = [] # Stores filtered files
+
+        output_msg("Filelist (unfiltered): {}".format(unfiltered_files))
+
+        for i in unfiltered_files: # Filtering
+            if all(text not in os.path.splitext(i)[part] for text in check[0]):     # If one of the items in check[0] isn't in the filename/extension, filter
+                output_msg( "Filtered {} (doesn't include {})".format(i, check) )
+                continue
+            filtered_files.append(i)
+            
+        output_msg("Filelist (filtered): {}".format(filtered_files))
+
+        return filtered_files
+    return unfiltered_files # If the check doesn't exist, then return the unfiltered files list 
+
+
+def file_sort_key(file):
+    """
+    Given a directory with...
+    "test1.txt, test2.txt, test10.txt"
+    The os.listdir() will return... 
+    "test1.txt, test10.txt, test2.txt"
+    To make %n work, we need it to return the files in a logical/natural form.
+    We use this key to achive that, while also using the sorted() function.
+
+    Parameters: 
+        unsorted_files : list
+            A list of unsorted files
+    """
+
+    key = re.split(r'(\d+)', file)
+    key[1::2] = map(int, key[1::2])
+    return key
+
+
+def rename_files(name, files):
+    """
+    Changes the name of the files given.
+
+    Parameters:
+        name : str
+            A string following the name syntax (see name arguments in README.md)
+        files : list
+            A list of filenames
+    """
+
+    new_names = [] # List of new names that match the files parameter
+
+    # Checking for {n} in the name
+    if "{n}" not in name:
+        name = name + "{n}"
+
+    # Calculating names
+    for i in range(len(files)):
+        rename = name.format(n=i+1)
+
+        if args.overwrite_ext == False:
+            rename = rename + os.path.splitext(files[i])[1]
+
+        new_names.append(rename)
+
+    # Checking new names with user
+    print("Old name\tNew name")
+    print("--------\t--------")
+    for i in range(len(files)):
+        print(files[i], "\t", new_names[i])
+
+    if input("Are the filenames correct? (y/N) ").upper() == "Y":
+        for i in range(len(files)):
+            # Renaming
+            os.rename(files[i], new_names[i])
+            output_msg("Renamed {} to {}".format(files[i], new_names[i]))
+    else:
+        output_msg("Input not Y or y, aborting.")
+
+
+
+# Processing arguments
+
+parser = argparse.ArgumentParser(description="Command line mass renaming tool")
+
+# Filter arguments
+parser.add_argument("name", type=str, help="The name you want to rename the files to")
+parser.add_argument("-f", "--filetype", action="append", nargs="+", type=str, help="Filters to files that use the extentions specified")
+parser.add_argument("-i", "--includes", action="append", nargs="+", type=str, help="You should give this argument as a string, it does count file extensions (string)")
+
+# Other arguments
+parser.add_argument("--show-output", action="store_true", help="Flag for program output")
+parser.add_argument("--overwrite-ext", action="store_true", help="Overwrites the built-in feature to automatically add extensions, allowing you to change the extension in the name parameter")
+
+args = parser.parse_args()
+
+
+
+# Main
+
+files = os.listdir(os.getcwd())                 # All files in working directory
+
+files = filter_files(files, args.filetype, 1)   # Filtering based on extension
+files = filter_files(files, args.includes, 0)   # Filtering based on filename
+
+files = sorted(files, key=file_sort_key)        # Sorting the files into human/natural order
+
+rename_files(args.name, files)
+
+
+###### To do ######
+# {a}             #
+###################
+
+
